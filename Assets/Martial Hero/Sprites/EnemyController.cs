3e05fc35c91edf55b0ff52c59cc1c02f8cd7f93d
Badguuuuuy <ab6699ni@gmail.com>
2024-03-07 19:35:48 +0900

    as

---
+++ Assets/Martial Hero/Sprites/EnemyController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int hp = 1;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        if (hp > 0)
+        {
+            hp--;
+            animator.SetTrigger("TakeHit");
+        }
+        else return;
+    }
+}
